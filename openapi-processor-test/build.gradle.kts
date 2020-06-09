30fcbdcbd505e8277d6e618820660cc25c07f412
Martin Hauner <martin.hauner@gmx.net>
2020-06-09 09:20:36 +0200

    fixed dependencies

--- openapi-processor-test/build.gradle.kts
+++ openapi-processor-test/build.gradle.kts
@@ -30,15 +30,18 @@ dependencies {
     compileOnly("com.github.hauner.openapi:openapi-processor-api:1.0.0")
 
     implementation("org.codehaus.groovy:groovy:2.5.12")
+    implementation("org.codehaus.groovy:groovy-nio:2.5.12")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.11.0")
     implementation("io.github.java-diff-utils:java-diff-utils:4.7")
-    api("com.google.jimfs:jimfs:1.1")
+    implementation("com.google.jimfs:jimfs:1.1") {
+        exclude("com.google.guava")
+    }
 
+    compileOnly("junit:junit:4.13")
     implementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
     runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
     runtimeOnly("org.junit.vintage:junit-vintage-engine:5.6.2")
-    compileOnly("junit:junit:4.13")
 }
 
 val sourcesJar by tasks.registering(Jar::class) {
