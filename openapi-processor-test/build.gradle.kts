95f754864d7761eca3a0bf0ada9753ddbb0f18d5
Martin Hauner <martin.hauner@gmx.net>
2022-05-09 08:27:01 +0200

    clean up

--- openapi-processor-test/build.gradle.kts
+++ openapi-processor-test/build.gradle.kts
@@ -14,9 +14,6 @@ group = projectGroupId
 version = projectVersion
 
 java {
-//    sourceCompatibility = JavaVersion.VERSION_1_8
-//    targetCompatibility = JavaVersion.VERSION_1_8
-
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(11))
     }
