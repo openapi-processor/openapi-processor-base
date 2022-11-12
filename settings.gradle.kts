152d4bdc3cc66e63dcbd4ae48b27edfe45e84c5e
Martin Hauner <martin.hauner@gmx.net>
2022-11-12 18:38:10 +0100

    fix included projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,7 @@ rootProject.name = "openapi-processor-base"
 
 include("openapi-processor-core")
 include("openapi-processor-test")
+include("openapi-processor-annotations")
+//include("slf4j-messager")
+//include("openapi-processor-tps")
+//include("openapi-processor-sample")
