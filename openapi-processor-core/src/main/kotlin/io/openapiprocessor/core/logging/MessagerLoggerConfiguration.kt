2b710f6622ac4c830699b63d3b217ac6e8c2ee0d
Martin Hauner <martin.hauner@gmx.net>
2022-11-13 15:03:47 +0100

    convert to kotlin

--- openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/logging/MessagerLoggerConfiguration.kt
+++ openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/logging/MessagerLoggerConfiguration.kt
@@ -2,69 +2,55 @@
  * Copyright 2022 https://github.com/openapi-processor/openapi-processor-base
  * PDX-License-Identifier: Apache-2.0
  */
+package io.openapiprocessor.core.logging
 
-package io.openapiprocessor.core.logging;
+import org.slf4j.event.Level
+import java.io.IOException
+import java.util.*
 
-import org.slf4j.event.Level;
+/**
+ * log leve can be configured with `messagerlogger.properties`. It does allow a single property:
+ * ```
+ * log.level=error|warn|info|debug|trace
+ * ```
+ */
+class MessagerLoggerConfiguration {
+    private var initialized = false
+    private val properties = Properties()
 
-import java.io.IOException;
-import java.util.Properties;
+    private var logLevel = Level.INFO
 
-public class MessagerLoggerConfiguration {
-    private static final String CONFIG_FILE_NAME = "messagerlogger.properties";
-
-    private boolean initialized = false;
-
-    private final Properties properties = new Properties();
-
-    private Level logLevel = Level.INFO;
-
-
-    void init() {
+    fun init() {
         if (initialized) {
-            return;
+            return
         }
-
-        loadProperties ();
-        initLogLevel ();
-        initialized = true;
+        loadProperties()
+        initLogLevel()
+        initialized = true
     }
 
-    Level getLogLevel() {
-        return logLevel;
-    }
+    private fun initLogLevel() {
+        val ll = properties.getProperty("log.level") ?: return
 
-    private void initLogLevel () {
-        String ll = properties.getProperty ("log.level");
-        if (ll == null) {
-            return;
-        }
-
-        switch (ll.toUpperCase ()) {
-            case "ERROR":
-                logLevel = Level.ERROR;
-                break;
-            case "WARN":
-                logLevel = Level.WARN;
-                break;
-            case "INFO":
-                logLevel = Level.INFO;
-                break;
-            case "DEBUG":
-                logLevel = Level.DEBUG;
-                break;
-            case "TRACE":
-                logLevel = Level.TRACE;
-                break;
+        when (ll.uppercase(Locale.getDefault())) {
+            "ERROR" -> logLevel = Level.ERROR
+            "WARN" -> logLevel = Level.WARN
+            "INFO" -> logLevel = Level.INFO
+            "DEBUG" -> logLevel = Level.DEBUG
+            "TRACE" -> logLevel = Level.TRACE
         }
     }
 
-    private void loadProperties () {
+    private fun loadProperties() {
         try {
-            ClassLoader classLoader = Thread.currentThread ().getContextClassLoader ();
-            properties.load (classLoader.getResourceAsStream (CONFIG_FILE_NAME));
-        } catch (IOException ignored) {
+            val classLoader = Thread.currentThread().contextClassLoader
+            properties.load(classLoader.getResourceAsStream(CONFIG_FILE_NAME))
+        } catch (ignored: IOException) {
             // ignore, use defaults
         }
+    }
+
+    companion object {
+        private const val CONFIG_FILE_NAME = "messagerlogger.properties"
     }
 }
