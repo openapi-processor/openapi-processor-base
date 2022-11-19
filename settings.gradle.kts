845d4f60c11a72543e11098453b5b9543aa2699a
Martin Hauner <martin.hauner@gmx.net>
2022-11-19 18:34:21 +0100

    remove annotations library

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,4 @@ rootProject.name = "openapi-processor-base"
 
 include("openapi-processor-core")
 include("openapi-processor-test")
-include("openapi-processor-annotations")
-//include("slf4j-messager")
-//include("openapi-processor-tps")
-//include("openapi-processor-sample")
+//include("openapi-processor-annotations")
