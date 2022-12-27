86f9cd1e66f9259b0a884e7652b152da67c00821
Martin Hauner <martin.hauner@gmx.net>
2022-12-27 19:20:20 +0100

    #2, enable integration test

--- openapi-processor-core/src/testInt/kotlin/io/openapiprocessor/core/ProcessorTestSets.kt
+++ openapi-processor-core/src/testInt/kotlin/io/openapiprocessor/core/ProcessorTestSets.kt
@@ -15,6 +15,7 @@ val ALL_30: List<TestSet> = listOf(
     TestSet("bean-validation-list-item-import", API_30),
     TestSet("bean-validation-requestbody", API_30),
     TestSet("bean-validation-requestbody-mapping", API_30),
+    TestSet("components-requestbodies", API_30),
     TestSet("deprecated", API_30),
     TestSet("endpoint-exclude", API_30),
     TestSet("endpoint-http-mapping", API_30), // framework specific
@@ -74,6 +75,7 @@ val ALL_31: List<TestSet> = listOf(
     TestSet("bean-validation-list-item-import", API_31),
     TestSet("bean-validation-requestbody", API_31),
     TestSet("bean-validation-requestbody-mapping", API_31),
+    TestSet("components-requestbodies", API_31),
     TestSet("deprecated", API_31),
     TestSet("endpoint-exclude", API_31),
     TestSet("endpoint-http-mapping", API_31), // framework specific
