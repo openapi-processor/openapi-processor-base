0ec155878388ca1be3f84f15e47b4c2e0413bd66
Martin Hauner <martin.hauner@gmx.net>
2022-12-28 12:23:39 +0100

    #6, extract "minimal" test case

---
+++ openapi-processor-core/src/testInt/resources/tests/swagger-parsing-error/generated/api/Api.java
@@ -0,0 +1,14 @@
+package generated.api;
+
+import annotation.Mapping;
+import annotation.Parameter;
+import generated.model.Foo;
+import generated.support.Generated;
+
+@Generated(value = "openapi-processor-core", version = "test")
+public interface Api {
+
+    @Mapping("/foo")
+    Foo postFoo(@Parameter Foo body);
+
+}
