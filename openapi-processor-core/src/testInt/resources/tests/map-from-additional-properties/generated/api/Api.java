c326384fb0b7679d3327d7834b7e89d0d98279d8
Martin Hauner <martin.hauner@gmx.net>
2023-01-29 17:16:36 +0100

    openapi-processor/openapi-processor-spring#150, add mappings with generics & nested generics

--- openapi-processor-core/src/testInt/resources/tests/map-from-additional-properties/generated/api/Api.java
+++ openapi-processor-core/src/testInt/resources/tests/map-from-additional-properties/generated/api/Api.java
@@ -2,11 +2,16 @@ package generated.api;
 
 import annotation.Mapping;
 import generated.support.Generated;
+import java.util.List;
+import java.util.Map;
 
 @Generated(value = "openapi-processor-core", version = "test")
 public interface Api {
 
     @Mapping("/foo")
     Map<String, String> getFoo();
+
+    @Mapping("/foo2")
+    Map<String, List<String>> getFoo2();
 
 }
