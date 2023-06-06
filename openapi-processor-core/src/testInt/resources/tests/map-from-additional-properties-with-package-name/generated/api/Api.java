3f831fd6c5c1015ee058802ddcc55e778d9aba93
Martin Hauner <martin.hauner@gmx.net>
2023-06-06 09:19:14 +0200

    #24, integration test
    (cherry picked from commit 5dc95367aa5256f5eaadd976e4fea633486d9a74)

---
+++ openapi-processor-core/src/testInt/resources/tests/map-from-additional-properties-with-package-name/generated/api/Api.java
@@ -0,0 +1,28 @@
+package generated.api;
+
+import annotation.Mapping;
+import generated.model.Value;
+import generated.support.Generated;
+import java.util.List;
+import java.util.Map;
+
+@Generated(value = "openapi-processor-core", version = "test")
+public interface Api {
+
+    /**
+     * query object dictionary
+     *
+     * @return dictionary response
+     */
+    @Mapping("/values")
+    Map<String, Value> getValues();
+
+    /**
+     * query object dictionary
+     *
+     * @return dictionary response
+     */
+    @Mapping("/multi-values")
+    Map<String, List<Value>> getMultiValues();
+
+}
