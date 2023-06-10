43801793228dab25f48d7cadc378197564e5d2e2
Martin Hauner <martin.hauner@gmx.net>
2023-06-10 11:48:14 +0200

    #25, should generate valid type name
    (cherry picked from commit 9014b543904559df08e1fd03025e0166e0af1d6e)

--- openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/writer/java/ModelClassNameCreator.kt
+++ openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/writer/java/ModelClassNameCreator.kt
@@ -13,13 +13,15 @@ import io.openapiprocessor.core.support.capitalizeFirstChar
 class ModelClassNameCreator(private val suffix: String) {
 
     fun createName(dataTypeName: String): String {
+        val classTypeName = toClass(dataTypeName)
+
         if (dataTypeName.isEmpty())
-            return dataTypeName
+            return classTypeName
 
         if (dataTypeName.endsWith(suffix.capitalizeFirstChar()))
-            return dataTypeName
+            return classTypeName
 
-        return "$dataTypeName${suffix.capitalizeFirstChar()}"
+        return "$classTypeName${suffix.capitalizeFirstChar()}"
     }
 
 }
