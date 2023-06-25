24170cd9c101722567606181b00f568180a32cd3
Martin Hauner <martin.hauner@gmx.net>
2023-06-26 01:15:00 +0200

    compile dependencies

---
+++ openapi-processor-core/src/testInt/resources/compile/Annotation.java
@@ -0,0 +1,15 @@
+package io.oap;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+@Documented
+@Retention(CLASS)
+@Target({FIELD})
+public @interface Annotation {
+    Class<?> value();
+}
