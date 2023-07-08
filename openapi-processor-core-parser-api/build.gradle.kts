9e92e502e181e7a93429389b3d1ed43a071db117
Martin Hauner <martin.hauner@gmx.net>
2023-07-08 18:23:17 +0200

    clean up

--- openapi-processor-core-parser-api/build.gradle.kts
+++ openapi-processor-core-parser-api/build.gradle.kts
@@ -1,5 +1,3 @@
-//@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
-
 plugins {
     id("openapiprocessor.library")
     id("openapiprocessor.publish")
@@ -10,21 +8,6 @@ repositories {
 }
 
 dependencies {
-//    compileOnly(libs.openapi.processor.api)
-//
-//    implementation(platform(libs.jackson.bom))
-//    implementation("com.fasterxml.jackson.core:jackson-databind")
-//    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
-//
-//    implementation(platform(libs.groovy.bom))
-//    implementation("org.apache.groovy:groovy")
-//    implementation("org.apache.groovy:groovy-nio")
-//
-//    implementation(libs.diff.utils)
-//    implementation(libs.jimfs) {
-//        exclude("com.google.guava")
-//    }
-//    implementation(libs.slf4j)
 }
 
 publishing {
