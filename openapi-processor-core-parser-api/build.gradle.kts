62c6c521dc9b29423c7b162333e8d7a2ac9b1f3b
Martin Hauner <martin.hauner@gmx.net>
2023-07-08 18:19:46 +0200

    extract openapi parser abstraction

---
+++ openapi-processor-core-parser-api/build.gradle.kts
@@ -0,0 +1,38 @@
+//@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+
+plugins {
+    id("openapiprocessor.library")
+    id("openapiprocessor.publish")
+    id("org.jetbrains.kotlin.jvm")
+}
+
+repositories {
+}
+
+dependencies {
+//    compileOnly(libs.openapi.processor.api)
+//
+//    implementation(platform(libs.jackson.bom))
+//    implementation("com.fasterxml.jackson.core:jackson-databind")
+//    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
+//
+//    implementation(platform(libs.groovy.bom))
+//    implementation("org.apache.groovy:groovy")
+//    implementation("org.apache.groovy:groovy-nio")
+//
+//    implementation(libs.diff.utils)
+//    implementation(libs.jimfs) {
+//        exclude("com.google.guava")
+//    }
+//    implementation(libs.slf4j)
+}
+
+publishing {
+    publications {
+        getByName<MavenPublication>("openapiprocessor") {
+            pom {
+                description.set("OpenAPI Processor Core Parser API")
+            }
+        }
+    }
+}
