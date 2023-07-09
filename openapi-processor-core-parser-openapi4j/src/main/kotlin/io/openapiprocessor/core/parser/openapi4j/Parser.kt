4fd58d7da6d05fa361b48260d8bfcf0d373924c7
Martin Hauner <martin.hauner@gmx.net>
2023-07-09 11:05:16 +0200

    implement service loader api

--- openapi-processor-core-parser-openapi4j/src/main/kotlin/io/openapiprocessor/core/parser/openapi4j/Parser.kt
+++ openapi-processor-core-parser-openapi4j/src/main/kotlin/io/openapiprocessor/core/parser/openapi4j/Parser.kt
@@ -6,21 +6,22 @@
 package io.openapiprocessor.core.parser.openapi4j
 
 import io.openapiprocessor.core.parser.ParserException
+import io.openapiprocessor.core.parser.Parser as ApiParser
+import io.openapiprocessor.core.parser.OpenApi as ApiOpenApi
 import org.openapi4j.core.exception.ResolutionException
 import org.openapi4j.core.validation.ValidationException
 import org.openapi4j.parser.OpenApi3Parser
 import org.openapi4j.parser.validation.v3.OpenApi3Validator
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import io.openapiprocessor.core.parser.OpenApi as ParserOpenApi
 
 /**
  * openapi4j parser.
  */
-open class Parser {
+open class Parser : ApiParser {
     private val log: Logger = LoggerFactory.getLogger(this.javaClass.name)
 
-    fun parse(apiPath: String): ParserOpenApi {
+    override fun parse(apiPath: String): ApiOpenApi {
         try {
             return run(apiPath)
         } catch (ex: ResolutionException) {
@@ -34,7 +35,7 @@ open class Parser {
         }
     }
 
-    private fun run(apiPath: String): ParserOpenApi {
+    private fun run(apiPath: String): ApiOpenApi {
         val api = OpenApi3Parser()
             .parse(toURL(apiPath), true)
 
@@ -44,5 +45,4 @@ open class Parser {
 
         return OpenApi(api, results)
     }
-
 }
---
+++ openapi-processor-core-parser-openapi4j/src/main/kotlin/io/openapiprocessor/core/parser/openapi4j/ParserProvider.kt
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2023 https://github.com/openapi-processor/openapi-processor-core
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiprocessor.core.parser.openapi4j
+
+import io.openapiprocessor.core.parser.Parser
+import io.openapiprocessor.core.parser.ParserProvider
+
+class ParserProvider : ParserProvider {
+    override fun getName(): String {
+        return "OPENAPI4J"
+    }
+
+    override fun getParser(): Parser {
+        return Parser()
+    }
+}
