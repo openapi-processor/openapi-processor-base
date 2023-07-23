6e4dab0e3e6732f6f498645867d041f2fcb45f77
Martin Hauner <martin.hauner@gmx.net>
2023-07-23 17:57:00 +0200

    extract swagger parser

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,6 @@ rootProject.name = "openapi-processor-base"
 include("openapi-processor-core")
 include("openapi-processor-core-parser-api")
 //include("openapi-processor-core-parser-internal")
-//include("openapi-processor-core-parser-swagger")
+include("openapi-processor-core-parser-swagger")
 include("openapi-processor-core-parser-openapi4j")
 include("openapi-processor-test")
