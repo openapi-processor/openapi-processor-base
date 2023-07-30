aa4f35b0c393485e259fc8f7f5d7e899d6f974f6
Martin Hauner <martin.hauner@gmx.net>
2023-07-30 16:32:51 +0200

    package should have its own HttpMethod

---
+++ openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/model/HttpMethod.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2019 https://github.com/openapi-processor/openapi-processor-base
+ * PDX-License-Identifier: Apache-2.0
+ */
+
+package io.openapiprocessor.core.model
+
+enum class HttpMethod(val method: String) {
+    GET ("get"),
+    PUT ("put"),
+    POST ("post"),
+    DELETE ("delete"),
+    OPTIONS ("options"),
+    HEAD ("head"),
+    PATCH ("patch"),
+    TRACE ("trace")
+}
