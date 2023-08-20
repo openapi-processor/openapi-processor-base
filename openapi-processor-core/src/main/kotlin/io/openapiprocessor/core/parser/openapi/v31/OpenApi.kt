8cd9b066c29022042c686e4e8271629ff7ea7a40
Martin Hauner <martin.hauner@gmx.net>
2023-08-20 15:50:50 +0200

    import alias consistency

--- openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/parser/openapi/v31/OpenApi.kt
+++ openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/parser/openapi/v31/OpenApi.kt
@@ -7,27 +7,26 @@ package io.openapiprocessor.core.parser.openapi.v31
 
 import io.openapiparser.model.v31.OpenApi as OpenApi31
 import io.openapiparser.model.v31.PathItem as PathItem31
-import io.openapiprocessor.core.parser.Path
+import io.openapiprocessor.core.parser.Path as ParserPath
+import io.openapiprocessor.core.parser.OpenApi as ParserOpenApi
 import io.openapiprocessor.core.parser.RefResolver as ParserRefResolver
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import io.openapiprocessor.core.parser.OpenApi as ParserOpenApi
-import io.openapiprocessor.core.parser.openapi.v31.Path as Path31
 
 class OpenApi(
     private val api: OpenApi31
 ) : ParserOpenApi {
     private val log: Logger = LoggerFactory.getLogger(this.javaClass.name)
 
-    override fun getPaths(): Map<String, Path> {
-        val paths = linkedMapOf<String, Path>()
+    override fun getPaths(): Map<String, ParserPath> {
+        val paths = linkedMapOf<String, ParserPath>()
 
         api.paths?.pathItems?.forEach { (name: String, value: PathItem31) ->
             var path = value
             if (path.isRef) {
                 path = path.refObject
             }
-            paths[name] = Path31(name, path)
+            paths[name] = Path(name, path)
         }
 
         return paths
@@ -42,5 +41,4 @@ class OpenApi(
     override fun hasWarnings(): Boolean {
         return false
     }
-
 }
