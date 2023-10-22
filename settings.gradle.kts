73e7e51d60c93ed7051a335756000a5d0f0b63ca
Martin Hauner <martin.hauner@gmx.net>
2023-10-22 16:15:11 +0200

    avoid sonar compile warning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,3 +20,5 @@ include("openapi-processor-core-parser-api")
 include("openapi-processor-core-parser-swagger")
 include("openapi-processor-core-parser-openapi4j")
 include("openapi-processor-test")
+
+System.setProperty("sonar.gradle.skipCompile", "true")
