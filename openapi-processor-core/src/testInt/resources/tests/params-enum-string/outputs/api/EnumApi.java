73386f78e9f1d99c21aa07ef340022806ba40203
Martin Hauner <martin.hauner@gmx.net>
2023-10-30 17:08:50 +0100

    openapi-processor/openapi-processor-spring#198, test with validation

--- openapi-processor-core/src/testInt/resources/tests/params-enum-string/outputs/api/EnumApi.java
+++ openapi-processor-core/src/testInt/resources/tests/params-enum-string/outputs/api/EnumApi.java
@@ -3,16 +3,18 @@ package generated.api;
 import annotation.Mapping;
 import annotation.Parameter;
 import generated.support.Generated;
+import generated.support.validation.Values;
+import jakarta.validation.constraints.NotNull;
 
 @Generated(value = "openapi-processor-core", version = "test")
 public interface EnumApi {
 
     @Mapping("/endpoint")
     void getEndpoint(
-            @Parameter String foo,
-            @Parameter String bar);
+            @Parameter @NotNull @Values(values = {"foo", "foo-2", "foo-foo"}) String foo,
+            @Parameter @NotNull @Values(values = {"bar", "bar-2", "bar-bar"}) String bar);
 
     @Mapping("/endpoint-dashed")
-    void getEndpointDashed(@Parameter String fooFoo);
+    void getEndpointDashed(@Parameter @NotNull @Values(values = {"foo", "foo-2", "foo-foo"}) String fooFoo);
 
 }
