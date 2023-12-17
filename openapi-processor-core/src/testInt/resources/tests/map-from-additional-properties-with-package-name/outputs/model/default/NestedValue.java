b8ef7bd87b989ef095927c6f6dd5b3e74800c035
Martin Hauner <martin.hauner@gmx.net>
2023-12-17 18:37:13 +0100

    add nested object (#91)
    (cherry picked from commit 4a2064238fbb8f3e6d98f41613ce6178d81e2b8e)

--- openapi-processor-core/src/testInt/resources/tests/map-from-additional-properties-with-package-name/outputs/model/default/NestedValue.java
+++ openapi-processor-core/src/testInt/resources/tests/map-from-additional-properties-with-package-name/outputs/model/default/NestedValue.java
@@ -2,17 +2,12 @@ package generated.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import generated.support.Generated;
-import javax.validation.Valid;
 
 @Generated(value = "openapi-processor-core", version = "test")
-public class Value {
+public class NestedValue {
 
   @JsonProperty("text")
   private String text;
-
-  @Valid
-  @JsonProperty("nested")
-  private NestedValue nested;
 
   public String getText() {
     return text;
@@ -20,14 +15,6 @@ public class Value {
 
   public void setText(String text) {
     this.text = text;
-  }
-
-  public NestedValue getNested() {
-    return nested;
-  }
-
-  public void setNested(NestedValue nested) {
-    this.nested = nested;
   }
 
 }
--- openapi-processor-core/src/testInt/resources/tests/map-from-additional-properties-with-package-name/outputs/model/record/NestedValue.java
+++ openapi-processor-core/src/testInt/resources/tests/map-from-additional-properties-with-package-name/outputs/model/record/NestedValue.java
@@ -2,9 +2,7 @@ package generated.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import generated.support.Generated;
-import javax.validation.Valid;
 
 @Generated(value = "openapi-processor-core", version = "test")
-public record Value(
-    @JsonProperty("text") String text, @Valid @JsonProperty("nested") NestedValue nested) {
+public record NestedValue(@JsonProperty("text") String text) {
 }
