95865fb13a91fc42eca053009169bc025ef8633f
Martin Hauner <martin.hauner@gmx.net>
2024-06-07 08:58:06 +0200

    simpler naming (openapi-processor/openapi-processor-spring#271)

--- openapi-processor-core/src/testInt/resources/tests/annotation-mapping-class/outputs/model/record/Foo.java
+++ openapi-processor-core/src/testInt/resources/tests/annotation-mapping-class/outputs/model/record/Foo.java
@@ -3,11 +3,11 @@ package generated.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import generated.support.Generated;
 import io.oap.Annotation;
-import io.oap.AnnotationAtClass;
-import io.oap.AnnotationAtClassParameter;
+import io.oap.ClassAnnotation;
+import io.oap.Param;
 import io.oap.Bar;
 
-@AnnotationAtClass(value = AnnotationAtClassParameter.class, simple = 2)
+@ClassAnnotation(value = Param.class, simple = 2)
 @Generated(value = "openapi-processor-core", version = "test")
 public record Foo(
     @Annotation(value = Bar.class)
