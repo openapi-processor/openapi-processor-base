b2f99fdbfa5b26a3f3aa689d89ad6035fa83f121
Martin Hauner <martin.hauner@gmx.net>
2024-06-07 09:21:54 +0200

    fix expected import order (openapi-processor/openapi-processor-spring#271)

--- openapi-processor-core/src/testInt/resources/tests/annotation-mapping-class/outputs/model/default/Foo.java
+++ openapi-processor-core/src/testInt/resources/tests/annotation-mapping-class/outputs/model/default/Foo.java
@@ -3,9 +3,9 @@ package generated.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import generated.support.Generated;
 import io.oap.Annotation;
+import io.oap.Bar;
 import io.oap.ClassAnnotation;
 import io.oap.Param;
-import io.oap.Bar;
 
 @ClassAnnotation(value = Param.class, simple = 2)
 @Generated(value = "openapi-processor-core", version = "test")
