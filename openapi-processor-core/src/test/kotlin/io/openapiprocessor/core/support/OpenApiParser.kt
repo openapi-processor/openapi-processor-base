672b1e9a9e71a78c67c2c1d9c32528688f6b5957
Martin Hauner <martin.hauner@gmx.net>
2024-07-09 09:08:49 +0200

    improve OpenAPI parsing in tests

--- openapi-processor-core/src/test/kotlin/io/openapiprocessor/core/support/OpenApiParser.kt
+++ openapi-processor-core/src/test/kotlin/io/openapiprocessor/core/support/OpenApiParser.kt
@@ -1,16 +1,16 @@
 /*
- * Copyright 2020 https://github.com/openapi-processor/openapi-processor-core
+ * Copyright 2020 https://github.com/openapi-processor/openapi-processor-base
  * PDX-License-Identifier: Apache-2.0
  */
 
 package io.openapiprocessor.core.support
 
-import io.openapiprocessor.core.parser.OpenApi as ParserOpenApi
 import io.openapiprocessor.core.parser.ParserType
 import io.openapiprocessor.core.parser.openapi.Parser
-import io.openapiprocessor.test.parser.swagger.parse as parseWithSwagger
-import io.openapiprocessor.test.parser.openapi4j.parse as parseWithOpenApi4j
 import io.openapiprocessor.test.stream.Memory
+import io.openapiprocessor.core.parser.OpenApi as ParserOpenApi
+import io.openapiprocessor.test.parser.openapi4j.parse as parseWithOpenApi4j
+import io.openapiprocessor.test.parser.swagger.parse as parseWithSwagger
 
 /**
  * OpenAPI parser to read yaml from memory using the given parser.
@@ -21,6 +21,26 @@ fun parseApi(apiYaml: String, parserType: ParserType = ParserType.INTERNAL): Par
     return parse(apiYaml, parserType)
 }
 
+fun parseApi(
+    header: String =
+        """
+        |openapi: 3.1.0
+        |info:
+        |  title: test API
+        |  version: 1.0.0
+        |
+        """,
+    body: String,
+    parserType: ParserType = ParserType.INTERNAL
+): ParserOpenApi {
+    val merged = (
+        header.trimMargin()
+      + body.trimMargin()
+    )
+    return parse(merged, parserType)
+}
+
+@Deprecated(message = "use parseApi(header, body, parser)")
 fun parse(apiYaml: String, parserType: ParserType = ParserType.SWAGGER): ParserOpenApi {
     return when (parserType) {
         ParserType.SWAGGER -> parseWithSwagger(apiYaml)
