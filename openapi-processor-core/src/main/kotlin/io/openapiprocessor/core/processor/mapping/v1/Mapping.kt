4fa27a4389919e5756a0424ebedda0fad51634bd
Martin Hauner <martin.hauner@gmx.net>
2024-07-22 00:29:49 +0200

    rework mappings lookup

--- openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/processor/mapping/v1/Mapping.kt
+++ openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/processor/mapping/v1/Mapping.kt
@@ -1,17 +1,6 @@
 /*
- * Copyright 2020 the original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2020 https://github.com/openapi-processor/openapi-processor-base
+ * PDX-License-Identifier: Apache-2.0
  */
 
 package io.openapiprocessor.core.processor.mapping.v1
@@ -19,31 +8,9 @@ package io.openapiprocessor.core.processor.mapping.v1
 import io.openapiprocessor.core.processor.mapping.MappingVersion
 
 /**
- * *the* Schema of the mapping yaml
- *
- *  @author Martin Hauner
+ * the *old* Schema of the mapping yaml, replaced by mapping.v2
  */
-@Deprecated("replaced by mapping.v2")
-class Mapping(
-
-    /**
-     * version (currently optional)
-     */
-    val openapiProcessorSpring: String?,
-
-    /**
-     * general options
-     */
-    val options: Options = Options(),
-
-    /**
-     * the type mappings
-     */
-    val map: Map = Map()
-
-): MappingVersion {
-
+class Mapping: MappingVersion {
     override val v2: Boolean
         get() = false
-
 }
