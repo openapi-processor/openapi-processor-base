9c60808b50c6b6eabf51c6df7a5b61e6d23545f9
Martin Hauner <martin.hauner@gmx.net>
2025-05-02 17:23:05 +0200

    clean up

--- openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/support/URI.kt
+++ openapi-processor-core/src/main/kotlin/io/openapiprocessor/core/support/URI.kt
@@ -11,8 +11,8 @@ import java.nio.file.Paths
 /**
  * convert a source string to a valid URI.
  *
- * if the source is an uri string it converts it to a URI. If the source has no scheme it assumes
- * a local path and adds the file scheme (i.e. file:).
+ * if the source is an uri string, it converts it to a URI. If the source has no scheme it assumes
+ * a local path and adds the file scheme (i.e., file:).
  *
  * @param source source path or url
  * @return a URI to the given source
@@ -27,7 +27,7 @@ fun toURI(source: String): URI {
         // ignore
     }
 
-    // no scheme, assume file path
+    // no scheme, assume a file path
     return Paths.get(source)
         .normalize()
         .toAbsolutePath()
