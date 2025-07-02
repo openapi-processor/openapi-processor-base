5c1149e292be5ecb6a2cdd0f255ef57dc97080e3
Martin Hauner <martin.hauner@gmx.net>
2025-07-02 08:53:00 +0200

    sonatype requires new urls for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,9 @@ nexusPublishing {
         sonatype {
             username.set(publishUser)
             password.set(publishKey)
+
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
         }
     }
 }
