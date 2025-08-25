d327ba663a6a6bb107d6fe977f3b52f2ec0925f7
Martin Hauner <martin.hauner@gmx.net>
2025-08-25 09:32:03 +0200

    improve integration test, add opt-in check (#290)

--- openapi-processor-core/src/testInt/resources/tests/bean-validation-mapping-supported/outputs/api/Api.java
+++ openapi-processor-core/src/testInt/resources/tests/bean-validation-mapping-supported/outputs/api/Api.java
@@ -4,13 +4,18 @@ import annotation.Mapping;
 import annotation.Parameter;
 import annotation.Status;
 import generated.support.Generated;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import java.time.Year;
+import other.CustomInteger;
 
 @Generated(value = "openapi-processor-core", version = "test")
 public interface Api {
 
     @Status("204")
     @Mapping("/foo")
-    void getFoo(@Parameter Year year);
+    void getFoo(
+            @Parameter Year year,
+            @Parameter @DecimalMin(value = "1970") @DecimalMax(value = "2099") CustomInteger other);
 
 }
