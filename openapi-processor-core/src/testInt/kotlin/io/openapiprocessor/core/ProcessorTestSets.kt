b8018303be03c3720bfc133a6a50a412dbe30ffd
Martin Hauner <martin.hauner@gmx.net>
2025-09-26 18:30:01 +0200

    enable integration test (#302)

--- openapi-processor-core/src/testInt/kotlin/io/openapiprocessor/core/ProcessorTestSets.kt
+++ openapi-processor-core/src/testInt/kotlin/io/openapiprocessor/core/ProcessorTestSets.kt
@@ -89,6 +89,7 @@ val ALL_30: List<TestParams> = listOf(
     test30_DR("schema-composed-oneof-interface"),
     //test30_DR("schema-duplicate-by-refs"), // not supported
     test30_DR("schema-mapping"),
+    test30_DR("schema-unreferenced"),
     test30_DR("server-url"),
     test30_DR("swagger-parsing-error")
 )
@@ -175,6 +176,7 @@ val ALL_31: List<TestParams> = listOf(
     test31_DR("schema-composed-oneof-interface"),
     //test31_DR("schema-duplicate-by-refs"), // not supported
     test31_DR("schema-mapping"),
+    test31_DR("schema-unreferenced"),
     test31_DR("server-url"),
     test31_DR("swagger-parsing-error")
 )
